699aa3c08f977d6503a044ece1a54a3fb051fe18
s16942 <s16942@pjwstk.edu.pl>
2021-03-04 00:11:28 +0100

    fix extends, new views and upgrade css

--- jakubmeller/urls.py
+++ jakubmeller/urls.py
@@ -17,13 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from jakubmeller.views import fp
+from jakubmeller.views import fp, aboutme, skills, history
 #from jakubmeller.views import AddCVView
 
 urlpatterns = [
     #general
     path('', fp, name='fp'),
     path('admin/', admin.site.urls),
+    path('aboutme/', aboutme, name='aboutme'),
+    path('skills/', skills, name='skills'),
+    path('history/', history, name='history'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jakubmeller/views.py
+++ jakubmeller/views.py
@@ -5,5 +5,17 @@ from .models import Article, Technology
 
 @csrf_protect
 def fp(request):
+    return render(request, 'jakubmeller/fp.html')
+
+@csrf_protect
+def skills(request):
     technology = Technology.objects.all()
-    return render(request, 'jakubmeller/fp.html', {'technologies': technology})
+    return render(request, 'jakubmeller/skills.html', {'technologies': technology})
+
+@csrf_protect
+def aboutme(request):
+    return render(request, 'jakubmeller/aboutme.html')
+
+@csrf_protect
+def history(request):
+    return render(request, 'jakubmeller/history.html')
